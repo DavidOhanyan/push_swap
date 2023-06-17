de5d7fac3037eb0d374e7f69ecbbb9ee7bc51394
David_Ohanyan <davitoahnyan630@gmail.com>
2023-06-17 15:48:48 +0400

    push_swap

--- src/check_str_mat.c
+++ src/check_str_mat.c
@@ -6,7 +6,7 @@
 /*   By: dohanyan <dohanyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 04:18:56 by dohanyan          #+#    #+#             */
-/*   Updated: 2023/03/16 15:05:44 by dohanyan         ###   ########.fr       */
+/*   Updated: 2023/06/17 15:48:15 by dohanyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_check_str_2(char *str)
 	i = 0;
 	if (str[i] == '-' || str[i] == '+')
 			i++;
-	if (str[i] == '0' && str[i + 1] == '0')
+	if (str[i] == '0')
 	{
 		while (str[i] == '0' && str[i] != '\0')
 			i++;
